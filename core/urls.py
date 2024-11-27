a744e9dbf2d2422166b817d133abef662b860fcc
Sofiia Zholud <szholuds@gmail.com>
2024-11-27 22:16:05 +0200

    Add urls.py for core app and configure routing, update .gitignore

---
+++ core/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import (
+    AstronomyShowViewSet,
+    ShowSessionViewSet,
+    ReservationViewSet,
+    TicketViewSet,
+    ShowThemeViewSet,
+    PlanetariumDomeViewSet,
+)
+
+router = routers.DefaultRouter()
+
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("tickets", TicketViewSet)
+router.register("show_themes", ShowThemeViewSet)
+router.register("planetarium_domes", PlanetariumDomeViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "planetarium"
